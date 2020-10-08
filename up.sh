37db6950c100d31b75746636b1e36f3ad35bc445
chunche95 <paeste95.pb@gmail.com>
2020-10-09 00:42:03 +0200

    GitHub

---
+++ up.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git add css/*
+git nota "Style"
+git sube
+git add fonts/*
+git nota "Fonts"
+git sube
+
+git add images/*
+git nota "Images"
+git sube
+
+git add js/*
+git nota "JavaScript"
+git sube
+
+git add php/*
+git nota "PHP-Contact"
+git sube
+
+git add videos/*
+git nota "Videos"
+git sube
+
+git add index.html
+git nota "Main page"
+git sube
+
+git add README.md
+git nota "README"
+git sube
+
+git add up.sh
+git nota "GitHub"
+git sube
